61c1bc627ba518d09832dc1d88b404aed9e9afc8
Rushabh bramhade <rushabh.bramhade123@gmail.com>
2026-01-15 14:34:36 +0530

    folder created name OOP's

---
+++ Qustion/03_question.py
@@ -0,0 +1,20 @@
+# Q3️⃣ Print all elements of this list
+# nums = [10, 20, 30]
+
+nums = [10, 20, 30]
+
+for i in nums:
+    print(i)
+
+
+# from user 
+
+
+user_input = int(input("How many number do you want to store in list :"))
+store_input = []
+for i in range(user_input):
+    sorte_input = int(input(f"Enter the number on inder {i+1} :"))
+    store_input = [i]
+
+print(store_input)
+
