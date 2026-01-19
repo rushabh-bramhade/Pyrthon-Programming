af84126d9093a16dd2433023e9829ea9bd4f451e
Rushabh bramhade <rushabh.bramhade123@gmail.com>
2026-01-19 13:00:07 +0530

    creating the folder name strings

---
+++ DSA/strings/01_strings.py
@@ -0,0 +1,10 @@
+# This all of the String creation 
+
+string = "hello i am learning the python topic dsa string"
+string_2 = '\nThis is singal quotation '
+string_3 = '''\n multi lines quotation   '''
+name =  "My name is 'Rushabh' "
+name_2 = '\nMy name is "Rushabh" ' 
+print(string , string_2 , string_3)
+print(name,name_2)
+
