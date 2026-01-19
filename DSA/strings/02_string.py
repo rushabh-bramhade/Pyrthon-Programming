7d7d721ad77af4e6421c577c9225e90220372cc6
Rushabh bramhade <rushabh.bramhade123@gmail.com>
2026-01-19 14:05:27 +0530

    string file add

---
+++ DSA/strings/02_string.py
@@ -0,0 +1,4 @@
+#finding the length of the Strings 
+
+string = "python is best" # len() function also count the spaces betweens the strings
+print(len(string))
---
+++ DSA/strings/03_string.py
@@ -0,0 +1,30 @@
+# index this the poisition where characters assigned 
+# index syntax is []
+# example :
+string = "python"
+# print(string,"index :",string[0])
+# i = -1
+# while i <= len(string):
+#     print(string[-i])
+#     i += 1
+
+
+# 1 <= 5 -1 n i += 1
+# 2 <= 5 -2 o i += 1
+# 3 <= 5 -3 h i += 1
+# 4 <= 5 -4 t i += 1
+# 5 <= 5 -5
+
+
+
+
+
+
+
+# slicing [start : stop : step ]
+# start example 1 or 2 or 3
+#stop example 8 or 9 or 10
+# step example -1 or 1 or 2 or 3
+
+for i in string[ : : -1 ]:
+    print(i)
